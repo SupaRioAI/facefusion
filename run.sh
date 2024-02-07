2a8c12458267fab620ffa50c6217a882bbd12a54
cyanjin <chaunceyyann@gmail.com>
2024-02-07 16:46:58 -0500

    face swapper model init

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-for target in `ls /Users/cyan/MEGA/DF/Input/SigalAcon/**.mp4`
+source_lady="JulianaMU0"
+source_ext=".png"
+target_lady="Murka"
+target_ext=**.mp4
+output_dir="/Users/cyan/MEGA/DF/Output/$source_lady-$target_lady"
+mkdir "$output_dir"
+for target in /Users/cyan/MEGA/DF/Input/"${target_lady}"/${target_ext}
 do
-    echo "processing $target"
-    python app.py --execution-provider coreml --headless -s "/Users/cyan/MEGA/DF/Face/JulianaMU0.png" -o "/Users/cyan/MEGA/DF/Output/JulianaMU0-SigalAcon" -t $target;
+    echo processing "${target}"
+    python run.py --execution-provider coreml --headless -s "/Users/cyan/MEGA/DF/Face/${source_lady}${source_ext}" -o "$output_dir" -t "${target}";
 done
